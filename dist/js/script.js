59a0d35f158a7bc6d4d272753c66e61366a24b73
vavan19911987 <v.pulin@yahoo.com>
2021-12-04 01:07:08 +0300

    hgh

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function () {
+    "user strict";
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        // spaceBetween: 130,
+        autoplay: {
+            delay: 2000,
+        },
+        speed: 500,
+        slidesPwgGroup: 1,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        centeredSlides: false,
+        slidesPerView: 1,
+
+    });
+
+});
+
